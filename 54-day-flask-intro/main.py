392817c6f03839ff50088c37cb56d2cabede7971
melumilic <melumilic@gmail.com>
2022-08-23 22:03:53 -0700

    Add another decorator example

--- 54-day-flask-intro/main.py
+++ 54-day-flask-intro/main.py
@@ -1,7 +1,15 @@
 from flask import Flask
 
+
+def make_bold(function):
+    def wrapper():
+        return "<b>" + function() + "</b>"
+    return wrapper
+
+
 app = Flask(__name__)
 
 @app.route("/")
+@make_bold
 def hello_world():
     return "<p>Hello, World!</p>"
