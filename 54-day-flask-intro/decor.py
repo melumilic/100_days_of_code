5c215741e23beca023ecc73a69dac057630f8820
melumilic <melumilic@gmail.com>
2022-08-24 16:35:03 -0700

    Add decorator example with argument

--- 54-day-flask-intro/decor.py
+++ 54-day-flask-intro/decor.py
@@ -14,4 +14,11 @@ def measure_runtime(function):
 def function_test():
     print("w")
 
+def is_authenticated_decorator(function):
+    def wrapper(*args,**kwargs):
+        if args[0].is_logged_in == True:
+            function(args[0])
+    return wrapper
+
+    
 function_test()
