0007ce71342ac8fd2c25cf6519081b5bfb3a5f3d
melumilic <melumilic@gmail.com>
2022-08-24 23:17:58 -0700

    Add api with templating example

---
+++ 57-day-templating/application.py
@@ -0,0 +1,29 @@
+from curses import napms
+from flask import Flask
+from flask import render_template
+import datetime
+import requests
+import json
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def home():
+    cur_year = datetime.datetime.now().strftime("%Y")
+    return render_template("index.html", cur_year=cur_year)
+
+
+@app.route("/guess/<name>")
+def guess(name):
+    params_value = {"name":name}
+    response = requests.get(url=f"https://api.agify.io/",params=params_value)
+    age_json = response.json()
+    response = requests.get(url=f"https://api.genderize.io/",params=params_value)
+    gender_json = response.json()
+    return render_template(
+        "guess.html",
+        gender_guess=gender_json["gender"],
+        age_guess=age_json["age"],
+        person_name=name
+    )
