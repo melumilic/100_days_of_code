94f37f8e8e3994613551b6f516d644174c60c5eb
melumilic <melumilic@gmail.com>
2022-08-27 03:51:22 -0700

    Finish implementing posts in flask

--- 59-day-bootstrap-practice/post.py
+++ 59-day-bootstrap-practice/post.py
@@ -1,6 +1,6 @@
 class Post:
-    def __init__(self,blog_id,blog_title,blog_subtitle,blog_content) -> None:
-        self.blog_id = blog_id
+    def __init__(self,post_id,blog_title,blog_subtitle,blog_content) -> None:
+        self.post_id = post_id
         self.title = blog_title
         self.subtitle = blog_subtitle
         self.text_content = blog_content
