b37ebf4748772560011002151e67476bb1661f9b
melumilic <melumilic@gmail.com>
2022-08-30 05:04:48 -0700

    Hide nav buttons based on auth status

--- 68-day-authentication/application.py
+++ 68-day-authentication/application.py
@@ -66,7 +66,7 @@ class User(UserMixin, db.Model):
 
 @app.route("/")
 def home():
-    return render_template("index.html")
+    return render_template("index.html",logged_in=current_user.is_authenticated)
 
 
 @app.route("/register", methods=["POST", "GET"])
